16a467061405a87f2626c2aafb7f354f31b02a51
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-01 13:28:47 +0300

    clear old

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-
-const PORT = process.env.PORT || 3003;
-app.listen(PORT);
