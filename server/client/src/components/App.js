f71be9c3066ecec8b7ac1630cec7641104600408
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-01 21:42:41 +0300

    client auth, header, routing

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,12 +1,42 @@
-function App() {
+import React, { useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
+import Header from "./Header";
+import Landing from "./Landing";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+function App(props) {
+  useEffect(() => {
+    props.fetchUser();
+
+    console.log("Component has mounted!");
+    return () => {
+      console.log("Component will unmount!");
+      // Perform any clean-up operations here
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hi there</p>
-        <a href="/auth/google">Sign In With Google</a>
-      </header>
+    <div>
+      <BrowserRouter>
+        {/* materialize class */}
+        <div className="container">
+          {/*same thing */}
+          {true ? <Route path="/" component={Header} /> : <Header />}
+          {/*same thing */}
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default connect(null, actions)(App);
+{
+  /* <a href="/auth/google">Sign In With Google</a> */
+}
---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_USER } from "../actions/types";
+
+export default (state = null, action) => {
+  switch (action.type) {
+    case FETCH_USER:
+      if (Object.keys(action.payload.passport).length === 0) return false;
+      return action.payload;
+    default:
+      return state;
+  }
+};
