8608ec3ae92813c5e135cf72616c1bbdc8a9f371
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-02 18:45:40 +0300

    added express forwarding

--- server/README.md
+++ server/README.md
@@ -1,1 +1,3 @@
 # surver-server
+
+if any request made at endpoint not defined at express, assume it's to react-router
--- server/index.js
+++ server/index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+if ((process.env.NODE_ENV = "production")) {
+  // express will serve up production assets
+  app.use(express.static("client/build"));
+  //if no file inside build that express is looking for it goes to next lines, otherwise it serves file
+
+  const path = (await import("path")).default;
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
