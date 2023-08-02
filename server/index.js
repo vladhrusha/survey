dd28d0da555b2f45a325fd81dbe3300cbd497216
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-02 22:01:54 +0300

    try run serve react at express

--- server/index.js
+++ server/index.js
@@ -25,18 +25,17 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
-// if ((process.env.NODE_ENV = "production")) {
-//   const path = (await import("path")).default;
+if ((process.env.NODE_ENV = "production")) {
+  const path = (await import("path")).default;
 
-//   // express will serve up production assets
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-//   //if no file inside build that express is looking for it goes to next lines, otherwise it serves file
+  // express will serve up production assets
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  //if no file inside build that express is looking for it goes to next lines, otherwise it serves file
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-// commit
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+}
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
